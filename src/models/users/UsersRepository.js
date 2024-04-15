9cccfb6eb7728996d57104a2052c89c3ff3730fd
arthurborges <arthurborges.senai564@gmail.com>
2024-04-15 10:54:13 -0300

    Feat: Refactor user controller to use async/await

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -11,36 +11,45 @@ export default class UsersRepository {
     return allUsers;
   }
 
-  getUserById(id) {
-    const user = this.users.find((user) => user.id === id);
+ async getUserById(id) {
+    const user = await this.pg.oneOrNone("SELECT * FROM users WHERE id = $1", [id]);
+    console.log(user);
     return user;
   }
 
-  getUserByEmail(email) {
-    const user = this.users.find((user) => user.email === email);
+  async getUserByEmail(email) {
+    const user = await this.pg.oneOrNone("SELECT * FROM users WHERE email = $1", [email]);
     return user;
   }
 
-  createUser(user) {
-    this.users.push(user);
+  async createUser(user) {
+    await this.pg.none("INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)", [
+      user.id,
+      user.name,
+      user.email,
+      user.password,
+    ]);
     return user;
+  
   }
 
-  updateUser(id, name, email, password) {
-    const user = this.getUserById(id);
+  async updateUser(id, name, email, password) {
+    const user = await this.getUserById(id);
 
     if (!user) {
       return null;
     }
 
-    user.name = name;
-    user.email = email;
-    user.password = password;
+  const updatedUser = await this.pg.oneOrNone(
+      "UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *",
+      [name, email, password, id]
+    );
 
-    return user;
+    return updatedUser;
   }
 
-  deleteUser(id) {
-    this.users = this.users.filter((user) => user.id !== id);
+  async deleteUser(id) {
+    await this.pg.none("DELETE FROM users WHERE id = $1", [id]);
+    
   }
 }
