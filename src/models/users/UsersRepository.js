00704ea3a5e266e94c2f4ccd704bf382dfea2a73
arthurborges <arthurborges.senai564@gmail.com>
2024-04-17 14:30:32 -0300

    Update database configuration and fix typo in .env.example

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -6,23 +6,40 @@ export default class UsersRepository {
   }
 
   async getUsers() {
+    try {
     const allUsers = await this.pg.manyOrNone("SELECT * FROM users");
     console.log(allUsers);
     return allUsers;
+  } catch (error) {
+    console.log("Failed to get users", error);  
+    throw error;
+  }
+  
   }
 
  async getUserById(id) {
+  try {
     const user = await this.pg.oneOrNone("SELECT * FROM users WHERE id = $1", [id]);
     console.log(user);
     return user;
+  } catch (error) {
+    console.log("Failed to get user by id", error);
+    throw error;
   }
+ }
 
   async getUserByEmail(email) {
+    try{
     const user = await this.pg.oneOrNone("SELECT * FROM users WHERE email = $1", [email]);
     return user;
+  } catch (error) {
+    console.log("Failed to get user by email", error);
+    throw error;
+  }
   }
 
   async createUser(user) {
+    try {
     await this.pg.none("INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)", [
       user.id,
       user.name,
@@ -31,11 +48,15 @@ export default class UsersRepository {
     ]);
     return user;
   
+  } catch (error) {
+    console.log("Failed to create user", error);
+    throw error;
   }
+}
 
   async updateUser(id, name, email, password) {
     const user = await this.getUserById(id);
-
+    try {
     if (!user) {
       return null;
     }
@@ -46,10 +67,19 @@ export default class UsersRepository {
     );
 
     return updatedUser;
+  } catch (error) {
+    console.log("Failed to update user", error);
+    throw error;
   }
+}
 
   async deleteUser(id) {
+    try {
     await this.pg.none("DELETE FROM users WHERE id = $1", [id]);
     
+  } catch (error) {
+    console.log("Failed to delete user", error);
+    throw error;
   }
+}
 }
